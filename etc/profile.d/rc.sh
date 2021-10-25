c736f8d1a158be88d81d26cd340089d5694da63a
Neobee <88863739+iNeobee@users.noreply.github.com>
2021-10-25 19:07:34 +0700

    Create rc.sh

---
+++ etc/profile.d/rc.sh
@@ -0,0 +1,29 @@
+#
+## /etc/profile.d/rc.sh
+#
+
+# source $XDG_CONFIG_HOME/bash/bashrc | default: [[ -f ~/.bashrc ]] && . ~/.bashrc
+# [[ -f $HOME/.config/bash/bashrc ]] && . $HOME/.config/bash/bashrc
+[[ -f $XDG_CONFIG_HOME/bash/bashrc ]] && . $XDG_CONFIG_HOME/bash/bashrc
+
+# alias
+alias ls='lsd'
+alias cat='bat'
+alias startx='startx "$XDG_CONFIG_HOME/X11/xinitrc"'
+
+alias arduino-cli='arduino-cli --config-file $XDG_CONFIG_HOME/arduino15/arduino-cli.yaml'
+
+# ~/.wgetrc .wget-hsts
+alias wget='wget --hsts-file="$XDG_CACHE_HOME/wget-hsts"'
+# set the hsts-file variable with an absolute path 
+# as wgetrc does not support environment variables: 
+# echo hsts-file \= "$XDG_CACHE_HOME"/wget-hsts >> "$XDG_CONFIG_HOME/wgetrc"
+
+# PS1='[\u@\h \W]\$ '
+export PS1="\[\e[31m\] \[\e[000m\]\[\e[33m\]\W \[\e[000m\]\[\e[32m\]\\$ \[\e[000m\]\[\e[34m\]> \[\e[000m\]"
+export BAT_THEME="Nord"
+
+# sdkman
+export SDKMAN_DIR="$HOME/.local/bin/sdkman"
+[[ -s "$HOME/.local/bin/sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.local/bin/sdkman/bin/sdkman-init.sh"
+
