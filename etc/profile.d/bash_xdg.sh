737df734221f3454f922a29c5a91e0b4832a1d31
Neobee <88863739+iNeobee@users.noreply.github.com>
2021-10-25 18:54:57 +0700

    Create bash_xdg.sh

---
+++ etc/profile.d/bash_xdg.sh
@@ -0,0 +1,21 @@
+# Make bash follow the XDG_CONFIG_HOME convention
+_confdir=\${XDG_CONFIG_HOME:-\$HOME/.config}/bash
+if [ -d "$_confdir" ] &&  [ "\$0" = "bash" ]
+then
+    . "\$_confdir"/bash_profile
+    . "\$_confdir"/bashrc
+    HISTFILE=\$_confdir/bash_history
+fi
+unset _confdir
+
+if [ -s "\${XDG_CONFIG_HOME:-\$HOME/.config}/bash/bash_logout" ]
+then
+    . "\${XDG_CONFIG_HOME:-\$HOME/.config}/bash/bash_logout"
+fi
+
+# Setting zsh
+## System wide configuration (using xdg directories)
+if [[ -d "\${XDG_CONFIG_HOME:-\$HOME/.config}"/zsh ]]
+then
+        export ZDOTDIR=\${XDG_CONFIG_HOME:-\$HOME/.config}/zsh
+fi
